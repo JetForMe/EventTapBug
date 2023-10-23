3b2c9e4bb979ab3511aa4220076a3120a381c9ef
Rick M <rmann@latencyzero.com>
2023-10-23 15:10:52 -0700

    Initial Commit

---
+++ EventTapBug/EventTapBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventTapBugApp.swift
+//  EventTapBug
+//
+//  Created by Rick Mann on 2023-10-23.
+//
+
+import SwiftUI
+
+@main
+struct EventTapBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
